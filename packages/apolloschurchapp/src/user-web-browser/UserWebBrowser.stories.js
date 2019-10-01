4b3aeffa66f5dbb7a73bc557b990b919a95aca42
Michael Neeley <micneeley14@gmail.com>
2019-10-01 09:53:14 -0400

    removed modal story

--- packages/apolloschurchapp/src/user-web-browser/UserWebBrowser.stories.js
+++ packages/apolloschurchapp/src/user-web-browser/UserWebBrowser.stories.js
@@ -3,22 +3,11 @@ import { storiesOf } from '@apollosproject/ui-storybook';
 
 import UserWebBrowser from '.';
 
-storiesOf('ui-kit/UserWebBrowser', module)
-  .add('Example', () => (
-    <UserWebBrowser
-      modal={false}
-      navigation={{ getParam: (uri, fallback) => fallback }}
-      url={
-        'https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending'
-      }
-    />
-  ))
-  .add('Modal', () => (
-    <UserWebBrowser
-      navigation={{ getParam: (uri, fallback) => fallback }}
-      modal
-      url={
-        'https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending'
-      }
-    />
-  ));
+storiesOf('ui-kit/UserWebBrowser', module).add('Example', () => (
+  <UserWebBrowser
+    navigation={{ getParam: (uri, fallback) => fallback }}
+    url={
+      'https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending'
+    }
+  />
+));
