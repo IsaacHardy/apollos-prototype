e7da4dd92a393ac9e052c56ea94b96341552e848
Vincent Wilson <vince@classyh.at>
2019-10-14 06:41:30 -0400

    Fix linter

--- packages/apollos-ui-auth/src/SMS/VerificationConnected.js
+++ packages/apollos-ui-auth/src/SMS/VerificationConnected.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable react/no-unused-prop-types, react/jsx-handler-names */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Formik } from 'formik';
